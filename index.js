0cd3c165d1d99d7c14da0aaedbe2976482ba74ca
jakob svenningsson <jaksve@kth.se>
2018-01-26 12:11:17 +0100

    first

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var rimraf = require('rimraf');
 
 app.use(bodyParser.json());
-// respond with "hello world" when a GET request is made to the homepage
+
 app.get('/', function (req, res) {
   res.send('hello world')
 })
@@ -12,27 +13,27 @@ app.post('/', function (req, res) {
     const repo = req.body["repository"];
     const commit = req.body["head_commit"];
     const gitURL = repo["clone_url"];
-    const gitName = repo["name"];
+    const repoName = repo["name"];
     const sha = commit["id"];
     const statusURL = repo["statuses_url"];
-    cloneRepo(gitURL);
+    cloneRepo(gitURL, repoName);
     res.send('hello world')
 })
 
-app.listen(8080);
-
-const cloneRepo = (gitURL) => {
+const cloneRepo = (gitURL, repoName, branch) => {
     const { spawn } = require('child_process');
-    const git = spawn('git', ['clone', gitURL]);
+    console.log("deleteing " + `/tmp/${repoName}`)
+    rimraf.sync(`/tmp/${repoName}`);
+    const git = spawn('git', ['clone', '-b', branch, gitURL], { cwd: '/tmp/' });
     git.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
-      });
-      
-      git.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-      });
-      
-      git.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
+    });  
+    git.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+    git.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
 }
+
+app.listen(8080);
